3360e59f78609c64e2124a50df36bfc9f28b3aed
ZubarevaEkaterina <zuboreva74@mail.ru>
2017-04-30 19:00:06 +0500

    n

--- University/Authorization.cs
+++ University/Authorization.cs
@@ -12,9 +12,34 @@ namespace University
 {
     public partial class Authorization : Form
     {
+        private string login;
+        private string password;
+
         public Authorization()
         {
             InitializeComponent();
+        }
+
+        private void Authorization_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            User check = new User();
+            check.Authorize(login, password);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+            login = textBox1.Text;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            password = textBox2.Text;
         }
     }
 }
---
+++ University/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+
+namespace University
+{
+    class User
+    {
+        public void Authorize(string login, string password)
+        {
+           
+
+            }
+
+        }
+    }
