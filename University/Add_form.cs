f7149aa3aae4c70af2ca323ebf416eec68378adc
ZubarevaEkaterina <zuboreva74@mail.ru>
2017-05-05 17:43:00 +0500

    n

--- University/Add_form.cs
+++ University/Add_form.cs
@@ -15,18 +15,76 @@ namespace University
         public Add_form()
         {
             InitializeComponent();
+         
+            Connection database = new Connection();
+
+            database.QueryExecuteReader("SELECT Group.[Group_name] FROM[Group];");
+            while (database.reader.Read())
+            {
+                comboBox2.Items.Add(database.reader["Group_name"].ToString());
+            }
+
+            database.CloseConnection();
+
+            database.QueryExecuteReader("SELECT Subject.[subject_name] FROM[Subject];");
+            while (database.reader.Read())
+            {
+                comboBox1.Items.Add(database.reader["subject_name"].ToString());
+            }
+
+            database.CloseConnection();
+
+
+            database.QueryExecuteReader("SELECT Type.[Type] FROM[Type];");
+            while (database.reader.Read())
+            {
+                comboBox6.Items.Add(database.reader["Type"].ToString());
+            }
+
+            database.CloseConnection();
+
+            int[] days = { 1, 2, 3, 4, 5, 6, 7 };
+            comboBox5.Items.Add(days);
+
+            for (int i = 0; i < days.Length; i++)
+            {
+                comboBox5.Items.Add(days[i]);
+            }
+
+            int[] time = { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            for (int i = 0; i < time.Length; i++)
+            {
+                comboBox3.Items.Add(time[i]);
+            }
+
+
+            database.QueryExecuteReader("SELECT Cabinet.[number] FROM[Cabinet];");
+            while (database.reader.Read())
+            {
+                comboBox4.Items.Add(database.reader["number"].ToString());
+            }
+
 
         }
 
         private void textBox3_TextChanged(object sender, EventArgs e)
         {
-            
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Add_lesson add = new Add_lesson();
-            add.Create_les(Convert.ToInt32(textBox3.Text),textBox1.Text, Convert.ToInt32(textBox4.Text), textBox5.Text, textBox2.Text, textBox6.Text);
+           Add_lesson add = new Add_lesson();
+            add.Create_les(Convert.ToInt32(comboBox5.Text), comboBox1.Text, Convert.ToInt32(comboBox3.Text), comboBox4.Text, comboBox6.Text, comboBox2.Text);
         }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void Message (string text)
+        { var res = MessageBox.Show(text); }
     }
 }
--- University/Add_lesson.cs
+++ University/Add_lesson.cs
@@ -10,30 +10,21 @@ namespace University
     {
         public Weekday day;
         public Group gr;
-       // public void Create_les(string group)
-        /*{
-            var Add_Lesson = new Add_lesson()
-            {
-                day = new Weekday()
-
-            };*/
-            Connection database = new Connection();
-            //  database.QueryExecuteReader("SELECT Lesson.cabinet, Lesson.type, Lesson.Weekday, Subject.subject_name, Lesson.Time, Subject.Teacher_name FROM Subject INNER JOIN Lesson ON Subject.[id] = Lesson.[subject] WHERE Subject.[group] = '" + group + "'");
-
-
-
-      //  }
-
+      
         public void Create_les(int day, string subject, int time, string cabinet, string type, string group)
-        {/*
-
+        {
+            int id_subject = 0;
+            
+/*
             var Add_Lesson = new Add_lesson()
             {
-                day = new Weekday(),
+                
                 gr = new Group()
 
             };
 
+            Add_Lesson.gr.name = group;
+            
             Add_Lesson.day.day = day1;
             Add_Lesson.day.lesson.time.time = time;
             Add_Lesson.day.lesson.subject.subject = subject;
@@ -42,10 +33,23 @@ namespace University
             Add_Lesson.gr.name = group;*/
 
             Connection database = new Connection();
-            database.QueryExecuteNon("INSERT INTO [Lesson] (cabinet, type, weekday, subject, time )VALUES('" + cabinet + "', '" + type + "', '" + day + "', '" + subject + "', '" + time+"');");
+            database.QueryExecuteReader("SELECT id FROM Subject WHERE Subject.subject_name = '"+ subject + "' and Subject.group = '"+ group +"';");
+
+            while (database.reader.Read())
+            {
+                id_subject = database.reader.GetInt32(0);
+            }
+            database.reader.Close();
             database.CloseConnection();
 
-            
+            database.QueryExecuteNon("INSERT INTO Lesson(cabinet, type, weekday, subject, [time]) VALUES('" + cabinet + "', '" + type + "', " + day + ", " + id_subject + ", " + time+");");
+            database.CloseConnection();
+
+            Add_form add = new Add_form();
+            add.Message("Событие создано");
+            Schedule sc = new Schedule();
+
+            sc.GetSchedule(group);
         }
     }
 }
