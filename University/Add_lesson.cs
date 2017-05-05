466d240ffa6d98e8f08aac7cecdc8350cfb9d889
ZubarevaEkaterina <zuboreva74@mail.ru>
2017-05-05 17:46:29 +0500

    все

--- University/Add_lesson.cs
+++ University/Add_lesson.cs
@@ -8,30 +8,11 @@ namespace University
 {
     class Add_lesson
     {
-        public Weekday day;
-        public Group gr;
       
         public void Create_les(int day, string subject, int time, string cabinet, string type, string group)
         {
             int id_subject = 0;
             
-/*
-            var Add_Lesson = new Add_lesson()
-            {
-                
-                gr = new Group()
-
-            };
-
-            Add_Lesson.gr.name = group;
-            
-            Add_Lesson.day.day = day1;
-            Add_Lesson.day.lesson.time.time = time;
-            Add_Lesson.day.lesson.subject.subject = subject;
-            Add_Lesson.day.lesson.cabinet.number = cabinet;
-            Add_Lesson.day.lesson.type.type = type;
-            Add_Lesson.gr.name = group;*/
-
             Connection database = new Connection();
             database.QueryExecuteReader("SELECT id FROM Subject WHERE Subject.subject_name = '"+ subject + "' and Subject.group = '"+ group +"';");
 
